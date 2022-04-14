fdfa2a19cad2d2b860de59382e37131e935c1ff5
lflannery <lflannery@seattletimes.com>
2022-04-14 15:01:04 -0700

    first

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+// var paywall = require("./lib/paywall");
+// setTimeout(() => paywall(12345678), 5000);
+
+require("component-responsive-frame/child");
+
+function findAncestor (el, sel) {
+    while ((el = el.parentElement) && !((el.matches || el.matchesSelector).call(el,sel)));
+    return el;
+}
+
+document.querySelectorAll(".toggleBox button").forEach(el => el.addEventListener('click', () => {
+  let toggleBox = findAncestor(el, ".toggleBox");
+  toggleBox.querySelectorAll("button, .iframe").forEach(el => el.classList.remove("active"));
+  // toggleBox.querySelectorAll(".iframe").forEach(el => el.classList.remove("active");
+  let getClass = el.classList[0];
+  // let toggleBox = findAncestor(el, ".toggleBox");
+  toggleBox.querySelectorAll(`.iframe.${getClass}, button.${getClass}`).forEach(el => el.classList.add("active"));
+}));
